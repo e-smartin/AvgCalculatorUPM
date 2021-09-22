e8e4016d68bc3e3e6d5bbcdac0db74fecda6a053
e-smartin <enriquesalinask8@hotmail.com>
2021-09-22 20:26:44 +0200

    fixed small error

--- meansPDF.py
+++ meansPDF.py
@@ -10,23 +10,27 @@ def main ():
     for i in range(pdfR.numPages -1):
         page = pdfR.getPage(i)
         s = page.extractText()
-        s = s.split("Curso")[1]
+        s = s.split("Calificación")[1]
         s = re.sub("(\d{4}-\d{2})","a",s)
         s = re.sub("General \d{1}","a",s)
         s = re.sub("Apto","Apto)",s)
         s = re.sub("ReconocimientoCred","ReconocimientoCred)",s)
         s = s.split(")")
+        m = s[:]
         for line in s:
             if "NOTA" in line or "ReconocimientoCred" in line or "Apto" in line:
-                s.remove(line)
+                m.remove(line)
 
-        s = "\n".join(s)
+        #join the string without the lines deleted
+        s = "\n".join(m)
         s = re.sub(",",".",s)
-        s = re.findall("\d{1}\.{1}\d{1}|10|\d{1}", s)
+        s = re.findall("\d{1}\.{1}\d{1}|10|12|\d{1}", s)
         ts+=s
 
-    #calculate the mean for each year
+    #convert to float 
     ts = [float (i) for i in ts]
+    
+    #calculate the mean 4 each year
     ects,tot = [0,0,0,0],[0,0,0,0]
     for i in range(0,len(ts)-2,3):
         ects[int(ts[i])-1] += ts[i+1]
@@ -34,8 +38,9 @@ def main ():
     
     #print the results
     for i in range(len(ects)):
+        pass
         print(str(i+1)+"º: "+ '%.2f' % (tot[i]/ects[i]))
-
+    print("Total: "+ '%.2f' % (sum(tot)/sum(ects)))
 
 if __name__ == "__main__":
     main()
